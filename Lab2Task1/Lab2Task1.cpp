b1e2d571c8c6d7380343f5437b3d240015825235
Yehor <egorkorsun7@gmail.com>
2023-09-15 12:27:47 +0200

    Добавьте файлы проекта.

---
+++ Lab2Task1/Lab2Task1.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+using namespace std;
+int SigmaSum(int from, int to) {
+	int sum = 0;
+	for (size_t i = from; i <= to; i++)
+	{
+		sum += i;
+	}
+	return sum;
+}
+
+int main()
+{
+	int M;
+	int N;
+
+	cout << "enter M" << endl;
+	cin >> M;
+
+	do
+	{
+		cout << "enter N" << endl;
+		cin >> N;
+		if (N == 0) {
+			cout << "N cannot be 0" << endl;
+		}
+	} while (N==0);
+	
+	int i = N * -1;
+	int j = 0;
+
+	int firstSum = SigmaSum(i,M);
+	int secondSum = SigmaSum(j,N);
+
+	cout << firstSum * secondSum * (1/ pow(i+j,2));
+}
---
+++ Lab2Task2/Lab2Task2.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+int Max(int arr[], size_t size) {
+    int max = arr[0];
+
+    for (size_t i = 1; i < size; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+
+    return max;
+}
+
+int main()
+{
+    int myArray[] = { 5, 2, 9, 1, 8 };
+
+    size_t arraySize = sizeof(myArray) / sizeof(myArray[0]);
+
+    cout << Max(myArray,arraySize);
+}
